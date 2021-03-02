207c08ea2cb03ea8a568de38cb25c1f0ef39e17d
robertbaum <46323639+robertbaum@users.noreply.github.com>
2021-03-02 17:39:13 +0300

    Update getAppData.ts

--- src/app/getAppData.ts
+++ src/app/getAppData.ts
@@ -1,15 +1,15 @@
+import { fetchMenu, normalizeMenu } from '../menu';
 import get from 'lodash/get';
 import isEmpty from 'lodash/isEmpty';
 import pick from 'lodash/pick';
-import MenuItem from 'react-storefront-connector/MenuItem';
+import MenuItem from 'react-storefront-connector/MenuItem'
 import AppData from 'react-storefront-connector/AppData';
-import { fetchMenu, normalizeMenu } from '../menu';
 
 function normalizeMenuItems(items: any[]): MenuItem[] {
   if (isEmpty(items)) {
     return null;
   }
-  return items.map((item) => ({
+  return items.map(item => ({
     text: get(item, 'name'),
     as: `/s${get(item, 'url')}`,
     href: '/s/[...categorySlug]',
@@ -18,7 +18,7 @@ function normalizeMenuItems(items: any[]): MenuItem[] {
 }
 
 function getTabs(menu: MenuItem): MenuItem[] {
-  const items: MenuItem[] = menu.items || [];
+  const items: MenuItem[] = get(menu, 'items', []);
   return items.map((item: MenuItem): MenuItem => ({
     ...pick(item, ['text', 'href', 'as']),
     items: (get(item, 'items') || []).map((subcategoryItem: MenuItem): MenuItem => ({
